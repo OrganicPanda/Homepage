4c647bff56e99e289a2b15b0f503b340f2fad14b
Steve Thomson <organicpanda@gmail.com>
2013-09-18 13:08:52 +0100

    Pulling the PandaCarousel input handling in to a plugin

--- src/script/PandaCarousel.f84afb0d0c2bc469344b36b358cc5c1c2ff5f7a9.js
+++ src/script/PandaCarousel.f84afb0d0c2bc469344b36b358cc5c1c2ff5f7a9.js
@@ -36,46 +36,6 @@ function PandaCarousel(element, options) {
 		}
 	};
 
-	// Pointers encapsulate the other stuff so check for them
-	if (window.navigator.msPointerEnabled) {
-
-		this.events = [
-
-			// Events for starting
-			"MSPointerDown",
-
-			// Events for continuing
-			"MSPointerMove",
-
-			// Events for ending
-			"MSPointerUp", "MSPointerCancel", "MSPointerOut"
-
-		];
-
-	} else {
-
-		this.events = [
-
-			// Events for starting
-			"touchstart", "mousedown",
-
-			// Events for continuing
-			"touchmove", "mousemove",
-
-			// Events for ending
-			"touchend", "touchcancel", "touchleave", "mouseup", "mouseleave", "mouseout"
-
-		];
-
-	}
-
-	this.lastXY = {};
-	this.distanceXY = {};
-	this.axisLocked = false;
-	this.AXIS_X = 0;
-	this.AXIS_Y = 1;
-	this.axis = this.AXIS_X;
-
 	// We are now live
 	this.addClass('pandacarousel-live');
 
@@ -91,7 +51,7 @@ function PandaCarousel(element, options) {
 	// Thanks to bespoke.js (A much better carousel, but you didn't hear that from me!) for this idea
 	if (typeof options.plugins != 'undefined') {
 		for (var plugin in options.plugins) {
-			options.plugins[plugin](this);
+			new options.plugins[plugin](this, options);
 		}
 	}
 
@@ -447,15 +407,6 @@ PandaCarousel.prototype.initEvents = function() {
 
 	var _this = this;
 
-	for (var e = 0, el = this.events.length; e < el; e++) {
-		this.listeners.push([this.element, this.events[e],
-			function(e) {
-				_this.handleEvent(e);
-			},
-			false
-		]);
-	}
-
 	this.listeners.push([window, 'resize',
 		function(e) {
 			_this.updateDimensions();
@@ -528,166 +479,6 @@ PandaCarousel.prototype.destroy = function() {
 	// Finally remove our reference to the main carousel element
 	// After this nothing will work properly
 	this.element = null;
-
-};
-
-// ne to: http://blogs.msdn.com/b/ie/archive/2011/10/19/handling-multi-touch-and-mouse-input-in-all-browsers.aspx
-PandaCarousel.prototype.handleEvent = function(eventObject) {
-
-	// If we have an array of changedTouches, use it, else create an array of one with our eventObject
-	var touchPoints = (typeof eventObject.changedTouches != 'undefined') ? eventObject.changedTouches : [eventObject];
-	for (var i = 0; i < touchPoints.length; ++i) {
-
-		var touchPoint = touchPoints[i];
-
-		// Pick up the unique touchPoint id if we have one or use 1 as the default
-		var touchPointId = (typeof touchPoint.identifier != 'undefined') ? touchPoint.identifier : (typeof touchPoint.pointerId != 'undefined') ? touchPoint.pointerId : 1;
-
-		if (eventObject.type.match(/(down|start)$/i)) {
-
-			// Process mousedown, MSPointerDown, and touchstart
-			this.lastXY[touchPointId] = {
-				x: touchPoint.pageX,
-				y: touchPoint.pageY
-			};
-			this.startDraw(eventObject, touchPointId, touchPoint.pageX, touchPoint.pageY, 0, 0);
-
-		} else if (eventObject.type.match(/move$/i)) {
-
-			// Process mousemove, MSPointerMove, and touchmove
-			if (this.lastXY[touchPointId] && !(this.lastXY[touchPointId].x == touchPoint.pageX && this.lastXY[touchPointId].y == touchPoint.pageY)) {
-				var cacheLastXY = this.lastXY[touchPointId];
-				this.lastXY[touchPointId] = {
-					x: touchPoint.pageX,
-					y: touchPoint.pageY
-				};
-				this.extendDraw(
-					eventObject,
-					touchPointId,
-					touchPoint.pageX,
-					touchPoint.pageY,
-					touchPoint.pageX - cacheLastXY.x,
-					touchPoint.pageY - cacheLastXY.y);
-			}
-
-		} else if (eventObject.type.match(/(cancel|leave|up|end|lost|out)$/i)) {
-
-			if (this.lastXY[touchPointId]) {
-
-				// Mouseout needs to be verified first as it can fire when passing in to/out of child elements
-				if (eventObject.type === "mouseout") {
-
-					var relTarget = eventObject.relatedTarget;
-
-					// Exit early if we haven't actually left the element
-					if (this.element === relTarget || relTarget === null) {
-						return;
-					}
-					while (relTarget && relTarget !== this.element) {
-						relTarget = relTarget.parentNode;
-					}
-					if (this.element === relTarget) {
-						return;
-					}
-
-				}
-
-				// Process mouseup, MSPointerUp, and touchend
-				delete this.lastXY[touchPointId];
-				this.endDraw(eventObject, touchPointId);
-
-			}
-
-		}
-
-	}
-
-};
-
-PandaCarousel.prototype.startDraw = function(eventObject, id, x, y) {
-
-	// Turn animation off during draw
-	this.stopAnimation();
-
-	// Start recording the distance
-	this.distanceXY[id] = {
-		x: 0,
-		y: 0
-	};
-
-};
-
-PandaCarousel.prototype.extendDraw = function(eventObject, id, x, y, deltaX, deltaY) {
-
-	// Add on the movement
-	this.distanceXY[id].x += deltaX;
-	this.distanceXY[id].y += deltaY;
-
-	if ((this.distanceXY[id].x > 5 || this.distanceXY[id].x < -5) && this.axisLocked == false) {
-
-		this.axis = (Math.abs(this.distanceXY[id].y) > Math.abs(this.distanceXY[id].x)) ? this.AXIS_Y : this.AXIS_X;
-
-		this.axisLocked = true;
-
-	}
-
-	// Only take action if the axis is locked to X
-	if (this.axisLocked && this.axis == this.AXIS_X) {
-
-		// Stop panning and zooming so we can draw
-		if (eventObject.preventManipulation) {
-			eventObject.preventManipulation();
-		}
-
-		// We are handling this event
-		if (eventObject.preventDefault) {
-			eventObject.preventDefault();
-		}
-
-		this.setRelativeOffset(-((deltaX / this.cacheElementWidth) * 100));
-
-	}
-
-};
-
-PandaCarousel.prototype.endDraw = function(eventObject, id) {
-
-	// Only take action if the axis is locked to X
-	if (this.axisLocked && this.axis == this.AXIS_X) {
-
-		// Stop panning and zooming so we can draw
-		if (eventObject.preventManipulation) {
-			eventObject.preventManipulation();
-		}
-
-		// We are handling this event
-		if (eventObject.preventDefault) {
-			eventObject.preventDefault();
-		}
-
-		// Did we move enough?
-		if (((Math.abs(this.distanceXY[id].x) / this.cacheElementWidth) * 100) > this.snapPercent) {
-
-			// Yes! Which way was the swipe?
-			// Move in that direction
-			if (this.distanceXY[id].x > 0) {
-				this.previous();
-			} else {
-				this.next(); 
-			}
-
-		} else {
-
-			// No, stay on the current page
-			this.gotoPage(this.currentPage);
-
-		}
-
-	}
-
-	// End this touch
-	this.axisLocked = false;
-	delete this.distanceXY[id];
 
 };
 
